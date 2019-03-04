8e033d88c702dd49c08c9ad95c0c417c80bdf38d
Vincent Huijts <vincent@exmachina.nl>
2019-03-04 17:09:17 +0100

    start project section 2: lecture 29

---
+++ src/main/java/vnx/springcourse/petclinic/PetclinicApplication.java
@@ -0,0 +1,14 @@
+package vnx.springcourse.petclinic;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class PetclinicApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vnx/springcourse/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,17 @@
+package vnx.springcourse.petclinic;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.test.context.junit4.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetclinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
