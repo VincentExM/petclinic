730432debb52295b5f95513b514d52b9cb007e12
Vincent Huijts <vincent@exmachina.nl>
2019-03-19 14:48:17 +0100

    clean up request mapping

--- petclinic-web/src/main/java/vnx/springcourse/petclinic/controllers/VetController.java
+++ petclinic-web/src/main/java/vnx/springcourse/petclinic/controllers/VetController.java
@@ -3,10 +3,11 @@ package vnx.springcourse.petclinic.controllers;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
+	@RequestMapping({ "", "/", "/index", "/index.html" })
 	public String listVets() {
 
 		return "vets/index";
